2b448a6b0c11bf9b1ff3d409f509825ffdccbe2e
Kevin Su <kevinsu@outlook.co.nz>
2022-05-08 15:12:28 +1000

    main page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className = "main-body">
+        <h1>LEETCODE SOLUTIONS</h1>
+        <p>By Kevin Su</p>
+        <h2>Array/Hash Maps</h2>
+        <h2>Two Pointers</h2>
+        <h2>Sliding Window</h2>
+        <h2>Stacks</h2>
+        <h2>Binary Search</h2>
+        <h2>Linked List</h2>
+        <h2>Trees</h2>
+        <h2>Tries</h2>
+        <h2>Heap/Priority Queue</h2>
+        <h2>Backtracking</h2>
+        <h2>Graphs</h2>
+        <h2>Advanced Graphs</h2>
+        <h2>1D DP</h2>
+        <h2>2D DP</h2>
+        <h2>Greedy</h2>
+        <h2>Intervals</h2>
+        <h2>Math and Geometry</h2>
+        <h2>Bit Manipulation</h2>
+        <p>Inspired by <a href="https://neetcode.io/" target="_blank" >Neetcode</a></p>
+      </div>
     </div>
   );
 }
